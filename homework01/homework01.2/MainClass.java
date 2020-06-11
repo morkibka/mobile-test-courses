a73d7188e767946e6bd64f54d8bd35f63893647d
maksim katser <maksim.katser@appcraft.by>
2020-06-11 17:35:50 +0300

    homework1.2

---
+++ homework01/homework01.2/MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass {
+    private int class_number = 20;
+
+    public int getClassNumber() {
+        return class_number;
+    }
+}
---
+++ homework01/homework01.2/MainClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass{
+
+    @Test
+    public void testGetClassNumber() {
+
+        Assert.assertTrue("число меньше 45", this.getClassNumber() > 45);
+    }
+
+}
